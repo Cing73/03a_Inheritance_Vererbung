8e1031d258835c1c57fbe3ea4fe4392643ce5349
Cing73 <craig@vancleaf>
2022-07-22 14:21:28 +0200

    All

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,39 +1,15 @@
 package com.cc.java;
 
-public class Cat {
+public class Cat extends SuperCat{
     
-    private String name;
-    private String furColor;
-    private int age;
-    private boolean isFemale;
-
     private int counter;
    
-    public Cat(String name, String furColor, int age, boolean isFemale) {
-        this.name = name;
-        this.furColor = furColor;
-        this.age = age;
-        this.isFemale = isFemale;
-    }
-
-    /** Getter */
-    public String getStringAttributes(String flag) {
-        switch (flag) {
-            case "#name":
-                return name; 
-            case "#furColor":
-                return furColor; 
-            default:
-                return "ERROR: ???";
-        }
+    public Cat(String name, String furColor, int age) {
+        super(name, furColor, age);
     }
 
     public String getAge() {
-        if (isFemale) {
-           return checkEscaliationLevel(); 
-        } else {
-           return String.valueOf(age);
-        }
+        return checkEscaliationLevel();
     }
 
     private String checkEscaliationLevel() {
@@ -57,5 +33,6 @@ public class Cat {
 
 
 }
+
 
 
